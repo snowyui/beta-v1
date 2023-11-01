0fc25f4443ccc7dbd5d71b8b6f7dd8caf57eb91d
S.Watanabe <ocelot2828@gmail.com>
2023-11-01 22:17:52 +0900

    feat(types): CSS Value typed

--- _internal/src/types.ts
+++ _internal/src/types.ts
@@ -1,27 +1,86 @@
-export type ReturnType = Record<string, string>
+import type { CSSProperties } from 'react'
 
-export type StyleType = {
-  [key: string]: PropertyType
+// -------------------------- defined string Integer unit --//
+type CSSUnit =
+  | 'px'
+  | 'em'
+  | 'rem'
+  | '%'
+  | 'vh'
+  | 'vw'
+  | 'vmin'
+  | 'vmax'
+  | 'cm'
+  | 'mm'
+  | 'in'
+  | 'pt'
+  | 'pc'
+  | 'ex'
+  | 'ch'
+export type CSSNumericValue = '0' | `${number}${CSSUnit}`
+type CSSPropertiesValues =
+  | CSSNumericValue
+  | `${CSSNumericValue} ${CSSNumericValue}`
+  | `${CSSNumericValue} ${CSSNumericValue} ${CSSNumericValue}`
+  | `${CSSNumericValue} ${CSSNumericValue} ${CSSNumericValue} ${CSSNumericValue}`
+
+// ------------------------------- string Integer units type -//
+export interface CustomCSSProperties extends CSSProperties {
+  width?: CSSNumericValue
+  height?: CSSNumericValue
+  margin?: CSSPropertiesValues
+  padding?: CSSPropertiesValues
+  fontSize?: CSSNumericValue
+  lineHeight?: CSSNumericValue
+  letterSpacing?: CSSNumericValue
+  wordSpacing?: CSSNumericValue
+  borderWidth?: CSSNumericValue
+  borderRadius?: CSSNumericValue
+  top?: CSSNumericValue
+  right?: CSSNumericValue
+  bottom?: CSSNumericValue
+  left?: CSSNumericValue
+  maxWidth?: CSSNumericValue
+  maxHeight?: CSSNumericValue
+  minWidth?: CSSNumericValue
+  minHeight?: CSSNumericValue
+  flexBasis?: CSSNumericValue
+  gap?: CSSNumericValue
+  gridGap?: CSSNumericValue
+  gridColumnGap?: CSSNumericValue
+  gridRowGap?: CSSNumericValue
 }
 
+// ------------------------------- build and insert ----------//
+// ------------------------------- all strings ---------------//
+export type SerializeType = Record<string, string>
+
+// ------------------------------- nested string key in property ----------------------//
+export type ClassesObjectType = Record<string, CustomCSSProperties>
+// ------------------------------- nested return type ---------------------------------//
 export type ReturnStyleType<T> = { [key in keyof T]: string }
-export type ProxyClassName = { any: string | [key: string] }
+export type ProxyClassName = { [key: string]: string }
 
-type CSSProperties = React.CSSProperties
-export interface PropertyType extends CSSProperties {
-  link?: CSSProperties
-  visited?: CSSProperties
-  hover?: CSSProperties
-  active?: CSSProperties
-  firstChild?: CSSProperties
-  lastChild?: CSSProperties
-  firstLine?: CSSProperties
-  lastLine?: CSSProperties
-  firstLetter?: CSSProperties
-  lastLetter?: CSSProperties
-  before?: CSSProperties
-  after?: CSSProperties
+// ------------------------------- direct key and value property ----------------------//
+export interface PropertiesType extends CSSProperties {
+  link?: CustomCSSProperties
+  visited?: CustomCSSProperties
+  hover?: CustomCSSProperties
+  active?: CustomCSSProperties
+  firstChild?: CustomCSSProperties
+  lastChild?: CustomCSSProperties
+  firstLine?: CustomCSSProperties
+  lastLine?: CustomCSSProperties
+  firstLetter?: CustomCSSProperties
+  lastLetter?: CustomCSSProperties
+  before?: CustomCSSProperties
+  after?: CustomCSSProperties
 }
+
+export type NonNestObjectType = Record<
+  string,
+  (CSSNumericValue | CSSPropertiesValues) & string
+>
 
 export interface AnimationConfig {
   animates: {
