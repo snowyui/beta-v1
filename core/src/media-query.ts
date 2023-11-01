2296a31110068f2d8931efb44c96bb34e16831f5
S.Watanabe <ocelot2828@gmail.com>
2023-11-01 22:10:41 +0900

    feat: update import types

--- core/src/media-query.ts
+++ core/src/media-query.ts
@@ -1,15 +1,13 @@
-import type { PropertyType } from '../../_internal/src'
+import type { CustomCSSProperties } from '../../_internal/src'
 
 export const mediaQuery = (query: string, secondQuery?: string) => {
   if (secondQuery) {
-    return function (target: PropertyType): PropertyType {
-      return {
-        ['@media screen and (' + query + ') and (' + secondQuery + ')']: target
-      }
+    return function (target: CustomCSSProperties): CustomCSSProperties {
+      return { ['@media (' + query + ') and (' + secondQuery + ')']: target }
     }
   } else {
-    return function (target: PropertyType): PropertyType {
-      return { ['@media screen and' + '(' + query + ')']: target }
+    return function (target: CustomCSSProperties): CustomCSSProperties {
+      return { ['@media (' + query + ')']: target }
     }
   }
 }
