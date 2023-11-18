8f37657685a893ae00ff7febfcd0a58a69cd7621
S.Watanabe <ocelot2828@gmail.com>
2023-11-18 23:04:15 +0900

    feat(sheet.ts): change create.ts as sheet.ts

--- core/src/sheet.ts
+++ core/src/sheet.ts
@@ -4,26 +4,29 @@ import type {
   ClassesObjectType
 } from '../../_internal/src'
 import {
-  serializeCreate as serializer,
+  serializeSheet as serializer,
   buildIn,
   injectCSS,
-  isInDevelopment
+  isInDevelopment,
+  isWindowDefined
 } from '../../_internal/src'
 
 import module from './style.module.css'
 
-export function create<T extends ClassesObjectType | ProxyClassName>(
+export function sheet<T extends ClassesObjectType | ProxyClassName>(
   object: T
 ): ReturnStyleType<T> {
   const { styleSheet, base62Hash } = serializer(object as ClassesObjectType)
 
-  if (isInDevelopment) buildIn(styleSheet)
+  if (isInDevelopment && !isWindowDefined) {
+    buildIn(styleSheet)
+  }
+
+  if (isInDevelopment) injectCSS(base62Hash, styleSheet)
 
   return new Proxy<T>(object, {
     get: function (target, prop: string) {
       if (typeof prop === 'string' && prop in target) {
-        if (isInDevelopment)
-          injectCSS(object as ClassesObjectType, prop, base62Hash, styleSheet)
         return isInDevelopment
           ? prop + '_' + base62Hash
           : module[prop + '_' + base62Hash]
