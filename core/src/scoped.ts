93c54c8acd2c19a4e8adc8c509b43a3f92860884
S.Watanabe <ocelot2828@gmail.com>
2023-11-19 23:00:11 +0900

    feat(scoped.ts): add core and root

--- core/src/scoped.ts
+++ core/src/scoped.ts
@@ -2,10 +2,13 @@ import type {
   ClassesObjectType,
   ReturnStyleType,
   ProxyClassName,
-  NonNestObjectType
+  NonNestObjectType,
+  RootType
 } from '../../_internal/src'
 import { sheet } from './sheet'
 import { style } from './style'
+import { core } from './core'
+import { root } from './root'
 
 export class Scoped {
   static sheet<T extends ClassesObjectType | ProxyClassName>(
@@ -15,5 +18,11 @@ export class Scoped {
   }
   static style<T extends NonNestObjectType>(object: T): string {
     return style(object)
+  }
+  static core<T extends ClassesObjectType>(object: T): void {
+    return core(object)
+  }
+  static root<T extends RootType>(object: T): void {
+    return root(object)
   }
 }
