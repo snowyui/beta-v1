1f59880e66f4bb0950e66da97036de0c98c954bb
S.Watanabe <ocelot2828@gmail.com>
2023-11-20 10:59:00 +0900

    fix(inject-css.ts): update

--- _internal/src/utils/inject-css.ts
+++ _internal/src/utils/inject-css.ts
@@ -1,18 +1,55 @@
-import { isWindowDefined, isDocumentDefined } from '_internal/src'
-import { ClassesObjectType } from '_internal/src'
+import { isWindowDefined, isDocumentDefined } from '../index'
 
-export function injectCSS(object: ClassesObjectType, className: string) {
-  const targetElement = document.getElementsByClassName(className)[0]
-  if (
-    isWindowDefined &&
-    isDocumentDefined &&
-    targetElement instanceof HTMLElement
-  ) {
-    const rules = object[className]
+const styleSheets: Record<string, HTMLStyleElement> = {}
+const hashCache: Record<string, string> = {}
 
-    for (const [property, value] of Object.entries(rules)) {
-      const cssProperty = property.replace(/([A-Z])/g, '-$1').toLowerCase()
-      targetElement.style.setProperty(cssProperty, value)
+const isServer = !isWindowDefined || !isDocumentDefined
+
+function createStyleElement(hash: string): HTMLStyleElement | null {
+  if (isServer || document.getElementById(hash)) return null
+
+  const styleElement = document.createElement('style')
+  styleElement.setAttribute('id', hash)
+  styleElement.setAttribute('type', 'text/css')
+  styleSheets[hash] = styleElement
+  document.head.appendChild(styleElement)
+
+  return styleSheets[hash]
+}
+
+export function injectCSS(hash: string, sheet: string) {
+  styleCleanUp()
+  hashCache[hash] = hash
+  const styleElement = createStyleElement(hash)
+  if (styleElement == null) return
+
+  styleElement.textContent = sheet
+}
+
+function styleCleanUp() {
+  if (isServer) return
+
+  requestAnimationFrame(() => {
+    for (const hash in hashCache) {
+      const classElements = document.getElementsByClassName('_' + hash)
+      if (classElements.length === 0) {
+        removeStyleElement(hashCache[hash])
+      }
+    }
+  })
+}
+
+function removeStyleElement(hash: string) {
+  if (styleSheets[hash]) {
+    delete styleSheets[hash]
+
+    if (hashCache.hasOwnProperty.call(hashCache, hash)) {
+      delete hashCache[hash]
+    }
+
+    const styleElement = document.getElementById(hash)
+    if (styleElement) {
+      document.head.removeChild(styleElement)
     }
   }
 }
