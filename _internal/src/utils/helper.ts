5240953e0d4d1e153445f2ece44a63273a8d075c
S.Watanabe <ocelot2828@gmail.com>
2023-11-20 11:00:42 +0900

    refactor(*): isUnderDevelopment is change to isInDevelopment

--- _internal/src/utils/helper.ts
+++ _internal/src/utils/helper.ts
@@ -2,7 +2,7 @@ import path from 'path'
 
 export const isWindowDefined = typeof window !== 'undefined'
 export const isDocumentDefined = typeof document !== 'undefined'
-export const isUnderDevelopment = process.env.NODE_ENV !== 'development'
+export const isInDevelopment = process.env.NODE_ENV === 'development'
 
 const dir = (direname: string, relativePath: string) => {
   return path.join(direname, relativePath)
